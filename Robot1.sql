a465edea3a26e1200a2964e05170e34b65a2cb77
Omnia besharah <65514346+Omniabesharah@users.noreply.github.com>
2021-07-11 09:41:44 +0300

    Add files via upload

---
+++ Robot1.sql
@@ -0,0 +1,159 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: Robot1
+-- ------------------------------------------------------
+-- Server version	5.7.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `slider1`
+--
+
+DROP TABLE IF EXISTS `slider1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slider1` (
+  `idslider1` int(11) NOT NULL,
+  PRIMARY KEY (`idslider1`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slider1`
+--
+
+LOCK TABLES `slider1` WRITE;
+/*!40000 ALTER TABLE `slider1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `slider1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slider2`
+--
+
+DROP TABLE IF EXISTS `slider2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slider2` (
+  `idslider2` int(11) NOT NULL,
+  PRIMARY KEY (`idslider2`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slider2`
+--
+
+LOCK TABLES `slider2` WRITE;
+/*!40000 ALTER TABLE `slider2` DISABLE KEYS */;
+/*!40000 ALTER TABLE `slider2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slider3`
+--
+
+DROP TABLE IF EXISTS `slider3`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slider3` (
+  `idslider3` int(11) NOT NULL,
+  PRIMARY KEY (`idslider3`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slider3`
+--
+
+LOCK TABLES `slider3` WRITE;
+/*!40000 ALTER TABLE `slider3` DISABLE KEYS */;
+/*!40000 ALTER TABLE `slider3` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slider4`
+--
+
+DROP TABLE IF EXISTS `slider4`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slider4` (
+  `idslider4` int(11) NOT NULL,
+  PRIMARY KEY (`idslider4`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slider4`
+--
+
+LOCK TABLES `slider4` WRITE;
+/*!40000 ALTER TABLE `slider4` DISABLE KEYS */;
+/*!40000 ALTER TABLE `slider4` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slider5`
+--
+
+DROP TABLE IF EXISTS `slider5`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slider5` (
+  `idslider5` int(11) NOT NULL,
+  PRIMARY KEY (`idslider5`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slider5`
+--
+
+LOCK TABLES `slider5` WRITE;
+/*!40000 ALTER TABLE `slider5` DISABLE KEYS */;
+/*!40000 ALTER TABLE `slider5` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slider6`
+--
+
+DROP TABLE IF EXISTS `slider6`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slider6` (
+  `idslider6` int(11) NOT NULL,
+  PRIMARY KEY (`idslider6`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slider6`
+--
+
+LOCK TABLES `slider6` WRITE;
+/*!40000 ALTER TABLE `slider6` DISABLE KEYS */;
+/*!40000 ALTER TABLE `slider6` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-24 13:18:53
---
+++ action_page.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbServername="127.0.0.1";
+$dbSUsername="root";
+$dbPassword="1234";
+$dbName="Robot1";
+
+$conn = mysqli_connect($dbServername,$dbSUsername,$dbPassword,$dbName);
+
+?>
+
